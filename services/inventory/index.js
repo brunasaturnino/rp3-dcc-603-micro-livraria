4cef86fcae52848ad443fb6a562df52b40c91b98
brunasaturnino <brunasaturnino03@gmail.com>
2025-05-28 22:45:41 -0300

    Tarefa prática #1 - Microservices

--- services/inventory/index.js
+++ services/inventory/index.js
@@ -20,6 +20,12 @@ server.addService(inventoryProto.InventoryService.service, {
             products: products,
         });
     },
+
+    // NOVA FUNÇÃO ADICIONADA AQUI 
+    SearchProductByID: (payload, callback) => {
+        const found = products.find((product) => product.id == payload.request.id);
+        callback(null, found);
+    }
 });
 
 server.bindAsync('127.0.0.1:3002', grpc.ServerCredentials.createInsecure(), () => {
